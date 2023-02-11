448b47ef6abd3bb5665ba631ff25747ef77e5087
jz8132543 <i@dora.im>
2023-02-11 18:11:13 +0800

    test: autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -3,43 +3,75 @@ return {
     "windwp/nvim-autopairs",
     event = "VeryLazy",
     config = function()
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
       local npairs = require("nvim-autopairs")
       local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
+
       npairs.setup({
         check_ts = true,
-        fast_wrap = {
-          map = "<A-e>",
+        enable_check_bracket_line = false,
+        ts_config = {
+          lua = { "string" }, -- it will not add pair on that treesitter node
         },
-        enable_check_bracket_line = true,
       })
 
-      local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
       npairs.add_rules({
         Rule(" ", " "):with_pair(function(opts)
           local pair = opts.line:sub(opts.col - 1, opts.col)
-          return vim.tbl_contains({
-            brackets[1][1] .. brackets[1][2],
-            brackets[2][1] .. brackets[2][2],
-            brackets[3][1] .. brackets[3][2],
-          }, pair)
+          return vim.tbl_contains({ "()", "[]", "{}" }, pair)
         end),
+        Rule("( ", " )")
+          :with_pair(function()
+            return false
+          end)
+          :with_move(function(opts)
+            return opts.prev_char:match(".%)") ~= nil
+          end)
+          :use_key(")"),
+        Rule("{ ", " }")
+          :with_pair(function()
+            return false
+          end)
+          :with_move(function(opts)
+            return opts.prev_char:match(".%}") ~= nil
+          end)
+          :use_key("}"),
+        Rule("[ ", " ]")
+          :with_pair(function()
+            return false
+          end)
+          :with_move(function(opts)
+            return opts.prev_char:match(".%]") ~= nil
+          end)
+          :use_key("]"),
+        Rule("=", "", { "cpp", "rust", "go", "lua" })
+          :with_pair(cond.not_inside_quote())
+          :with_pair(function(opts)
+            local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+            if last_char:match("[%w%=%s]") then
+              return true
+            end
+            return false
+          end)
+          :replace_endpair(function(opts)
+            local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+            local next_char = opts.line:sub(opts.col, opts.col)
+            next_char = next_char == " " and "" or " "
+            if prev_2char:match("%w$") then
+              return "<bs> =" .. next_char
+            end
+            if prev_2char:match("%=$") then
+              return next_char
+            end
+            if prev_2char:match("=") then
+              return "<bs><bs>=" .. next_char
+            end
+            return ""
+          end)
+          :set_end_pair_length(0)
+          :with_move(cond.none())
+          :with_del(cond.none()),
       })
-      for _, bracket in pairs(brackets) do
-        npairs.add_rules({
-          Rule(bracket[1] .. " ", " " .. bracket[2])
-            :with_pair(function()
-              return false
-            end)
-            :with_move(function(opts)
-              return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-            end)
-            :use_key(bracket[2]),
-        })
-      end
     end,
   },
 }
