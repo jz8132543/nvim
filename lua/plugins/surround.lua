ea7b3b1108f198f99d51f103a38e6596c31daf06
jz8132543 <i@dora.im>
2023-02-11 19:08:09 +0800

    fix: which-key

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,49 @@
+return {
+  -- TODO: keymaps
+  {
+    "kylechui/nvim-surround",
+    keys = {
+      { "n", "ys" },
+      { "n", "yS" },
+      { "n", "cs" },
+      { "n", "cS" },
+      { "n", "ds" },
+      { "n", "dS" },
+      { "x", "gs" },
+      { "x", "gS" },
+      { "i", "<C-g>" },
+    },
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          visual = "gs",
+        },
+        surrounds = {
+          ["y"] = {
+            add = function()
+              local result = require("nvim-surround.config").get_input("Enter the type name: ")
+              if result then
+                return { { result .. "<" }, { ">" } }
+              end
+            end,
+            find = function()
+              return require("nvim-surround.config").get_selection({
+                pattern = "[^=%s%(%)]+%b<>",
+              })
+            end,
+            delete = "^(.-<)().-(>)()$",
+            change = {
+              target = "^.-([%w_]+)()<.->()()$",
+              replacement = function()
+                local result = require("nvim-surround.config").get_input("Enter new type replacement: ")
+                if result then
+                  return { { result }, { "" } }
+                end
+              end,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
