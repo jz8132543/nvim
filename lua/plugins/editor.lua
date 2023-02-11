04d72f33b179a32532a10516b74ad5ea7db403c3
jz8132543 <i@dora.im>
2023-02-11 19:08:34 +0800

    fix: which-key

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,25 +1,6 @@
 vim.g.neo_tree_remove_legacy_commands = 1
 
 return {
-  -- which-key
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
-      wk.setup({
-        show_help = false,
-        plugins = { spelling = true },
-        key_labels = { ["<leader>"] = "SPC" },
-      })
-      wk.register({
-        f = { name = "+file" },
-        g = { name = "+git" },
-        b = { name = "+buffer" },
-      }, { prefix = "<leader>" })
-    end,
-  },
-
   -- git signs
   {
     "lewis6991/gitsigns.nvim",
