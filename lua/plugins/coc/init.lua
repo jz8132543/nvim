ef081b09209c68d5d88625d734c4c9ff4126d432
jz8132543 <i@dora.im>
2023-02-15 18:06:18 +0800

    test: coc

--- lua/plugins/coc/init.lua
+++ lua/plugins/coc/init.lua
@@ -3,8 +3,21 @@ return {
   {
     "neoclide/coc.nvim",
     branch = 'release',
-    -- build = "yarn install --frozen-lockfile",
     lazy = false,
+    keys = {
+      -- Diagnostic
+      { '[e', '<Plug>(coc-diagnostic-prev)' },
+      { ']e', '<Plug>(coc-diagnostic-next)' },
+
+      -- LSP actions
+      { '<leader>cr', '<Plug>(coc-rename)', { desc = "Rename" } },
+      { '<leader>cf', '<Plug>(coc-fix-current)', { silent = true, desc = "Quickfix" } },
+      { '<leader>ca', '<Plug>(coc-codeaction)', { silent = true, desc = "Code Actions" } },
+      { '<leader>cf', '<cmd>call CocActionAsync("format")<cr>', desc = "Format" },
+      { 'gd', '<Plug>(coc-definition)', desc = "Definition" },
+      { 'gt', '<Plug>(coc-type-definition)', desc = "Type Definition" },
+      { 'K', "<cmd>call CocActionAsync('doHover')<cr>" },
+    },
   },
   { "rafcamlet/coc-nvim-lua" },
 }
--- lua/plugins/coc/servers.lua
+++ lua/plugins/coc/servers.lua
@@ -1,9 +1,18 @@
 return {
-    {
+  {
+    "iamcco/coc-diagnostic",
+    build = "yarn install --frozen-lockfile",
+  },
+  {
     "xiyaowong/coc-sumneko-lua",
     build = "yarn install --frozen-lockfile",
     ft = "lua",
   },
+  -- {
+  --   "xiyaowong/coc-stylua",
+  --   build = "yarn install --frozen-lockfile",
+  --   ft = "lua",
+  -- },
   {
     "neoclide/coc-json",
     build = "yarn install --frozen-lockfile",
