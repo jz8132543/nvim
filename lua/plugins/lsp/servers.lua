975ebfda561044fcc95e5a0bb6be1b402fa3dfda
jz8132543 <jz8132543@live.cn>
2023-04-28 05:21:33 +0000

    fix: diagnostics

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -34,6 +34,12 @@ local servers = {
       Lua = {
         workspace = {
           checkThirdParty = false,
+          library = {
+            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+            [vim.fn.stdpath("config") .. "/lua"] = true,
+          },
+          maxPreload = 10000,
         },
         hint = {
           enable = true,
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,8 +1,10 @@
 return {
   "mawkler/onedark.nvim",
   priority = 999,
+  name = "onedark",
   lazy = false,
   config = function()
     require("onedark").setup({})
+    vim.cmd.colorscheme("onedark")
   end,
 }
