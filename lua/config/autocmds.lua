96f534d3a5cd9f0a4aa9a4dfe1d864ee248e5de9
jz8132543 <jz8132543@live.cn>
2023-05-12 09:31:23 +0000

    fix: Auto create dir when saving

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,3 +42,14 @@ vim.api.nvim_create_autocmd("CursorHold", {
   end,
   group = diag_float_grp,
 })
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
