e9379896e0005c6e092c8a74664e90ecc0f0a508
jz8132543 <i@dora.im>
2023-05-18 17:34:57 +0800

    chore: add lsps on shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
-with import <nixpkgs> { };
-mkShell {
-  buildInputs = with pkgs; [
-    clang
-    clang-tools
-    clang-analyzer
-    sumneko-lua-language-server
-    stylua
-  ];
-  shellHook = ''
-    export CLANG=${pkgs.clang}/bin/clang
-    export CLANGD=${pkgs.clang-tools}/bin/clangd
-  '';
-}
+with import <nixpkgs> {};
+  mkShell {
+    buildInputs = with pkgs; [
+      sumneko-lua-language-server
+      stylua
+      nil
+      alejandra
+      taplo
+    ];
+  }
