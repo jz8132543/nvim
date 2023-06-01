f9e7c80b261e1e9bbbe88a22e42c0be34088fb3f
jz8132543 <i@dora.im>
2023-06-01 22:39:57 +0800

    chore: add mini.indentscope & fix cmp keymaps

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,12 +43,29 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
-    event = "VeryLazy",
+    event = { "BufReadPost", "BufNewFile" },
     config = {
       char = "▏",
       show_end_of_line = true,
       buftype_exclude = { "terminal", "nofile" },
     },
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "notify" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
 
   {
