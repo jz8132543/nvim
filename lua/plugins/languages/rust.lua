becf56a693b2643e61a32e18a1086e725fc3615e
jz8132543 <i@dora.im>
2024-03-07 17:19:17 +0800

    fix: lsp

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -1,45 +1,8 @@
 return {
   {
-    -- "simrat39/rust-tools.nvim",
-    "Ciel-MC/rust-tools.nvim",
+    -- TODO
+    "mrcjkb/rustaceanvim",
     event = { "BufReadPre", "BufNewFile" },
     ft = "rust",
-    opts = {
-      tools = {
-        runnables = {
-          use_telescope = true,
-        },
-      },
-      settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            features = "all",
-          },
-          checkOnSave = true,
-          check = {
-            command = "clippy",
-            features = "all",
-            extraArgs = { "--all", "--", "-W", "clippy::all" },
-          },
-          procMacro = {
-            enable = true,
-          },
-        },
-      },
-      server = {
-        on_attach = function(client, bufnr)
-          require("plugins.lsp.utils").on_attach(client, bufnr)
-          vim.keymap.set("n", "K", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
-          vim.keymap.set(
-            "n",
-            "<leader>ca",
-            require("rust-tools").code_action_group.code_action_group,
-            { buffer = bufnr }
-          )
-          vim.keymap.set("n", "<F5>", require("rust-tools").runnables.runnables, { buffer = bufnr })
-          vim.keymap.set("n", "<F6>", require("rust-tools").debuggables.debuggables, { buffer = bufnr })
-        end,
-      },
-    },
   },
 }
