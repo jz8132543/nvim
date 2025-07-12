d969e58bbf97467635cc920edf2be7f3e1a96be3
jz8132543 <i@dora.im>
2025-07-12 22:44:52 +0800

    fix cmp keymaps

--- lua/plugins/edit/cmp.lua
+++ lua/plugins/edit/cmp.lua
@@ -83,8 +83,8 @@ return {
           end,
         },
         mapping = {
-          ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-          ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+          ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
           ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
           ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
